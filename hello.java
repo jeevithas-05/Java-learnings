839a2d07b0d7ee661c3211419575956fcbd520ab
unknown <jeevithashankaregowda@gmail.com>
2026-02-10 12:51:44 +0530

    complete till user input

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to java programming.");
+    }
+}
+
+//this line is commented
---
+++ user_input.java
@@ -0,0 +1,50 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_int{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        System.out.println("The value of a is: "+a);
+    }
+}
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_int{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextLine();
+        System.out.println("The value of a is: "+name);
+    }
+}
+
+import java.lang.System;
+import java.util.Scanner;
+
+class user_input_int{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float f = scn.nextFloat();
+        System.out.println("The float is: "+f);
+    }
+}
+
+class user_input_float{
+
+}
+
+class addition_of_two_numbers{
+    public static void main(String[] args) {
+        scanner scn = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = scn.nextInt();
+        System.out.println("Enter second number:");
+        int b = scn.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of "+a+" and "+b+"  is:"+sum);
+    }
+}
+
+
+
